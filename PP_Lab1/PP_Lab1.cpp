03bba7f869d0a185185367027ab56090bd9a73b4
Matey Bogachev <boga4iov.matvei2016@yandex.ru>
2024-04-19 00:25:52 +0400

    написаны функции для записи, генерации и умножения матриц. в файл ResualExperiment.txt записывается время умножения матриц различныз размерностей

--- PP_Lab1/PP_Lab1.cpp
+++ PP_Lab1/PP_Lab1.cpp
@@ -1,7 +1,82 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <fstream>
+#include <direct.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+using namespace std::chrono;
+
+
+void writeMatrixToFile(const vector<vector<int>>& matrix, const string& filename) {
+    ofstream file(filename);
+    for (const auto& row : matrix) {
+        for (const auto& element : row) {
+            file << element << " ";
+        }
+        file << "\n";
+    }
+    file.close();
+}
+
+
+vector<vector<int>> generateMatrix(int size) {
+    vector<vector<int>> matrix(size, vector<int>(size));
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distrib(-100, 100);
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            matrix[i][j] = distrib(gen);
+        }
+    }
+
+    return matrix;
+}
+
+
+vector<vector<int>> multiplyMatrices(const vector<vector<int>>& A, const vector<vector<int>>& B) {
+    int n = A.size();
+    vector<vector<int>> C(n, vector<int>(n, 0));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    return C;
+}
+
+int main() {
+    _mkdir("data");
+
+    ofstream resultFile("data/ResultExperiment.txt");
+
+    vector<int> sizes = { 10, 50, 100, 500, 600, 800, 900, 1000, 1500 };
+
+    for (int size : sizes) {
+        auto A = generateMatrix(size);
+        auto B = generateMatrix(size);
+
+        writeMatrixToFile(A, "data/matrix1.txt");
+        writeMatrixToFile(B, "data/matrix2.txt");
+
+        auto start = high_resolution_clock::now();
+        cout << "Выполняются расчеты для матриц размером" << size << "X" << size;
+        auto C = multiplyMatrices(A, B);
+        auto stop = high_resolution_clock::now();
+
+        writeMatrixToFile(C, "data/matrixRes.txt");
+
+        auto duration = duration_cast<milliseconds>(stop - start);
+        resultFile << "Для матрицы размером " << size << "x" << size << " время " << duration.count() << " ms" << endl;
+        cout << "Для матрицы размером" << size << "x" << size << " расчесты выполнены";
+    }
+
+    resultFile.close();
+    return 0;
 }
